8317a1b2b87546b6361019f53965e71db5c74171
Rustam <mc4len@mail.ru>
2019-06-27 12:06:16 +0600

    Admin icons

--- resources/views/back/main-menu.blade.php
+++ resources/views/back/main-menu.blade.php
@@ -4,72 +4,72 @@
             <li class="header">Страницы редактирования</li>
             <li>
                 <a href="/adm/all">
-                    <i class="fa fa-circle-o"></i> <span>Общие настройки</span>
+                    <i class="fa fa-gear"></i> <span>Общие настройки</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/title-block">
-                    <i class="fa fa-circle-o"></i> <span>Титульный блок</span>
+                    <i class="fa fa-star"></i> <span>Титульный блок</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/about">
-                    <i class="fa fa-circle-o"></i> <span>О Brave</span>
+                    <i class="fa fa-child"></i> <span>О Brave</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/atmosphere">
-                    <i class="fa fa-circle-o"></i> <span>Атмосфера</span>
+                    <i class="fa fa-bolt"></i> <span>Атмосфера</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/signing-up">
-                    <i class="fa fa-circle-o"></i> <span>Запись</span>
+                    <i class="fa fa-edit"></i> <span>Запись</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/reviews">
-                    <i class="fa fa-circle-o"></i> <span>Отзывы</span>
+                    <i class="fa fa-comment"></i> <span>Отзывы</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/habit">
-                    <i class="fa fa-circle-o"></i> <span>Привычка</span>
+                    <i class="fa fa-tags"></i> <span>Привычка</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/challenge">
-                    <i class="fa fa-circle-o"></i> <span>Вызов</span>
+                    <i class="fa fa-bullhorn"></i> <span>Вызов</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/instagram">
-                    <i class="fa fa-circle-o"></i> <span>Инстаграм</span>
+                    <i class="fa fa-instagram"></i> <span>Инстаграм</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/excuses">
-                    <i class="fa fa-circle-o"></i> <span>Что мешает</span>
+                    <i class="fa fa-asterisk"></i> <span>Что мешает</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/offers">
-                    <i class="fa fa-circle-o"></i> <span>Вступление в Brave</span>
+                    <i class="fa fa-plus-square"></i> <span>Вступление в Brave</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/meta">
-                    <i class="fa fa-circle-o"></i> <span>Мета-данные</span>
+                    <i class="fa fa-chain"></i> <span>Мета-данные</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/post">
-                    <i class="fa fa-circle-o"></i> <span>Настройки почты</span>
+                    <i class="fa fa-envelope-o"></i> <span>Настройки почты</span>
                 </a>
             </li>
             <li>
                 <a href="/adm/scripts">
-                    <i class="fa fa-circle-o"></i> <span>Скрипты</span>
+                    <i class="fa fa-file-code-o"></i> <span>Скрипты</span>
                 </a>
             </li>
 
